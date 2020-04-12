73217e9cc3c4eeff2a54b181cc5749f4c6f4e318
Binup karki <karki011@gmail.com>
2020-04-11 23:30:34 -0500

    added view and displayed test text in homepage

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Search
+
 
 # Register your models here.
+
+class SearchAdmin(admin.ModelAdmin):
+    exclude = ('created',)
+
+
+admin.site.register(Search, SearchAdmin)
--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+
+
+class Search(models.Model):
+    search = models.CharField(max_length=255)
+    created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.search
+
+    class Meta:
+        verbose_name_plural = "Searches"
