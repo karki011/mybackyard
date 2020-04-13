2fb4de0dc0abfb72317fb910f8ac1f456385fbc5
Binup karki <karki011@gmail.com>
2020-04-13 14:33:08 -0500

    added requirement .txt

--- my_app/views.py
+++ my_app/views.py
@@ -1,13 +1,24 @@
+import requests
 from django.shortcuts import render
+from bs4 import BeautifulSoup
 # Create your views here.
+
+BASE_URL = 'https://bham.craigslist.org/search/sss?query={}'
 
 
 def home(request):
-    print('this is home')
+    print('view.home')
     return render(request, "base.html")
 
 
 def new_search(request):
-    print('this is new search')
-
-    return render(request, 'my_app/new_search.html', )
+    print('view.search')
+    search = request.POST.get('search')
+    response = requests.get(
+        'https://bham.craigslist.org/search/sss?query=python')
+    data = response.text
+    print(data)
+    frontend = {
+        'search': search
+    }
+    return render(request, 'my_app/new_search.html', frontend)
